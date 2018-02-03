52df8e6c59a8621a6dd8d5a4c913349415d48161
Som webster <somnathfreak007@gmail.com>
2018-02-03 04:13:05 +0000

    Create main2.js

---
+++ js/custom/main2.js
@@ -0,0 +1,9 @@
+function main(){
+    // var sprite = renderSprite(smoothedRoot);
+	// renderText("Full description \nof the item", sprite);    
+    //renderBox(smoothedRoot);
+     renderObject(smoothedRoot, "data/models/homefinal.json");
+     // renderObject(smoothedRoot, "http://ar-test-api.surge.sh/tree.json");
+    // renderObject(smoothedRoot, "data/models/scene.json");
+    
+}
