8af84b23127a6703cc2157d39082e1dcb10dcc92
Som webster <somnathfreak007@gmail.com>
2018-02-03 04:18:33 +0000

    Update main.js

--- js/custom/main.js
+++ js/custom/main.js
@@ -2,7 +2,7 @@ function main(){
     // var sprite = renderSprite(smoothedRoot);
 	// renderText("Full description \nof the item", sprite);    
     //renderBox(smoothedRoot);
-     renderObject(smoothedRoot, "data/models/homefinal.json");
+     renderObject(smoothedRoot, "data/models/model(4).json");
      // renderObject(smoothedRoot, "http://ar-test-api.surge.sh/tree.json");
     // renderObject(smoothedRoot, "data/models/scene.json");
     
